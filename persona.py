2eec460fc80fb7891454895fd5fab8c6c9849e73
Vanessa <vanessa.yepesg@udea.edu.co>
2021-11-06 12:45:15 -0500

    haciendo locuras

--- persona.py
+++ persona.py
@@ -12,3 +12,64 @@ class Persona():
 persona1=Persona('vanessa',22,20044004) #creo la persona 
 print(persona1.nombre)
 
+#defio el nombre de la clase 
+class Persona():
+
+    #CONTRUCTOR DE UNA CLASE
+    def __init__(self,nombre,edad, telefono,documento):  #init es el contractor de la clase
+        #atributos de la clase
+        self.nombre= nombre #siempre pongo el self.
+        self.edad= edad
+        self.telefono= telefono
+        self.documento=documento
+
+persona1=Persona('vanessa',22,20044004) #creo la persona 
+print(persona1.nombre)
+
+
+#defio el nombre de la clase 
+class Persona():
+
+    #CONTRUCTOR DE UNA CLASE
+    def __init__(self,nombre,edad, telefono,documento):  #init es el contractor de la clase
+        #atributos de la clase
+        self.nombre= nombre #siempre pongo el self.
+        self.edad= edad
+        self.telefono= telefono
+        self.documento=documento
+
+persona1=Persona('vanessa',22,20044004) #creo la persona 
+print(persona1.nombre)
+
+
+#defio el nombre de la clase 
+class Persona():
+
+    #CONTRUCTOR DE UNA CLASE
+    def __init__(self,nombre,edad, telefono,documento):  #init es el contractor de la clase
+        #atributos de la clase
+        self.nombre= nombre #siempre pongo el self.
+        self.edad= edad
+        self.telefono= telefono
+        self.documento=documento
+
+persona1=Persona('vanessa',22,20044004) #creo la persona 
+print(persona1.nombre)
+
+
+#defio el nombre de la clase 
+class Persona():
+
+    #CONTRUCTOR DE UNA CLASE
+    def __init__(self,nombre,edad, telefono,documento):  #init es el contractor de la clase
+        #atributos de la clase
+        self.nombre= nombre #siempre pongo el self.
+        self.edad= edad
+        self.telefono= telefono
+        self.documento=documento
+
+persona1=Persona('vanessa',22,20044004) #creo la persona 
+print(persona1.nombre)
+
+
+
